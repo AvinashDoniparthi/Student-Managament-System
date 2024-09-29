9eca28efe36c994c1e845300f63d2b3c7651c800
AvinashDoniparthi <152802906+AvinashDoniparthi@users.noreply.github.com>
2024-09-29 18:35:55 +0530

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,6 +1,9 @@
+#Python - mySQL integration
 import mysql.connector as mc
 con = mc.connect(host = 'localhost', user = 'root', password = 'avinashbvm', database = 'studb')
 cur = con.cursor()
+
+
 #database creation
 import dbCreation
 dbcreation.createdb()
